c697c4ee10ce415a2ed19260f58732eac263be4c
jennhoward <60079014+jennhoward@users.noreply.github.com>
2020-03-27 13:32:22 -0400

    committing project 3

---
+++ Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace IT1050Project3
+{
+    public class Instructor
+    {
+        private string Name;
+        private string CourseName;
+
+        public Instructor(string name, string courseName)
+        {
+            Name = name; 
+            CourseName = courseName;
+        }
+
+        public void SetStudentGrade(Student studentName, int studentGrade)
+        {
+            studentName.Grade += studentGrade;
+        }
+
+        public string Print => Name + " teaches " + CourseName;
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace IT1050Project3
+{
+    public class Student
+    {
+        private string Name;
+        private Instructor Teacher;
+        private int Grade;
+
+        public Student(string name, Instructor teacher)
+        {
+            Name = name;
+            Teacher = teacher;
+            Grade = 0;
+        }
+
+        public void SetGrade(int grade)
+        {
+            Grade += grade;
+        }
+
+        public string Print()
+        {
+            return $"{Name} has an " + Grade + " from " + Teacher.Print;
+        }
+    }
+}
